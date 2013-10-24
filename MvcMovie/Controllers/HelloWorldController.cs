36cf434af2ea1b518027b8623a05afbe89eb5c0d
qi chen <yurychika@gmail.com>
2013-10-24 23:14:22 +0800

    initial

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public ActionResult Index()
+        {
+            //return "This is my <b>default</b> action...";
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("hello " + name + " the number is " + numTimes);
+        }
+    }
+}
